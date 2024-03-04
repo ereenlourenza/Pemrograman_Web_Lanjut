c57b0f8098004807c648b9baf03555cae341b8c9
Ereen Lourenza Natalia Mamahi <ereenlourenza@gmail.com>
2024-03-04 22:28:12 +0700

    praktikum 5

---
+++ Pertemuan 3/PWL_POS/app/Http/Controllers/KategoriController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class KategoriController extends Controller
+{
+    public function index(){
+        // $data = [
+        //     'kategori_kode' => 'SNK',
+        //     'kategori_nama' => 'Snack/Makanan Ringan',
+        //     'created_at' => now(),
+        // ];
+        // DB::table('m_kategori')->INSERT($data);
+        // return 'Insert data baru berhasil';
+
+        // $row = DB::table('m_kategori')
+        // ->WHERE('kategori_kode', 'SNK')
+        // ->UPDATE(['kategori_nama' => 'Camilan']);
+        // return 'Update data berhasil. Jumlah data yang diupdate: ' .$row. ' baris';
+
+        // $row = DB::table('m_kategori')
+        // ->WHERE('kategori_kode', 'SNK')
+        // ->DELETE();
+        // return 'Delete data berhasil. Jumlah data yang dihapus: ' .$row. ' baris';
+
+        $data = DB::table('m_kategori')->get();
+        return view('kategori', ['data' => $data]);
+    }
+}
