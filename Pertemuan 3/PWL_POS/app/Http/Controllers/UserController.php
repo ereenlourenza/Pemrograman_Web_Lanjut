e55d7f1865fd611764464697982e438b4349adb7
Ereen Lourenza Natalia Mamahi <ereenlourenza@gmail.com>
2024-03-12 17:56:09 +0700

    js4_p2.1_retrieving single models

--- Pertemuan 3/PWL_POS/app/Http/Controllers/UserController.php
+++ Pertemuan 3/PWL_POS/app/Http/Controllers/UserController.php
@@ -9,24 +9,29 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        //tambah data user dengan Eloquent Model
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-        ];
-        UserModel::create($data);
-
-        //coba akses model UserModal
-        $user = UserModel::all(); //ambil semua data dari tabel m_user
+        // Ambil model dengan kunci utamanya...
+        $user = UserModel::where('level_id','>',20)->firstOr(function(){
+            abort(404);
+        });
         return view('user', ['data' => $user]);
-    }
-}
-
+        
+        // //tambah data user dengan Eloquent Model
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        // ];
+        // UserModel::create($data);
 
         // //update data user dengan Eloquent Model
         // $data = [
         //     'nama' => 'Pelanggan Pertama',
         // ];
         // UserModel::where('username', 'customer-1')->update($data); //update data user
+
+        // //coba akses model UserModal
+        // $user = UserModel::all(); //ambil semua data dari tabel m_user
+        // return view('user', ['data' => $user]);
+    }
+}
