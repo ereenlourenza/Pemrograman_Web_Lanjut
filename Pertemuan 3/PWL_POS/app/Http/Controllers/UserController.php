2b5f8eddd91bc3f366c7735af083f15a95766488
Ereen Lourenza Natalia Mamahi <ereenlourenza@gmail.com>
2024-03-14 13:13:27 +0700

    js4_p2.4_retreiving or Creating Models

--- Pertemuan 3/PWL_POS/app/Http/Controllers/UserController.php
+++ Pertemuan 3/PWL_POS/app/Http/Controllers/UserController.php
@@ -9,12 +9,54 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        // Ambil model dengan kunci utamanya...
-        $user = UserModel::where('level_id',2)->count();
-        // dd($user);
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ],
+        );
+        $user->save();
         return view('user',['data' => $user]);
+        // $user = UserModel::firstOrNew(
+        //     [
+        //         'username' => 'manager',
+        //         'nama' => 'Manager',
+        //     ],
+        // );
+        // return view('user',['data' => $user]);
+        // $user = UserModel::firstOrCreate(
+        //     [
+        //         'username' => 'manager22',
+        //         'nama' => 'Manager Dua Dua',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ],
+        // );
+        // return view('user',['data' => $user]);
+        // $user = UserModel::firstOrCreate(
+        //     [
+        //         'username' => 'manager',
+        //         'nama' => 'Manager'
+        //     ],
+        // );
+        // return view('user',['data' => $user]);
+
+        // Ambil model dengan kunci utamanya...
+
+        // $user = UserModel::where('level_id',2)->count();
+        // // dd($user);
+        // return view('user',['data' => $user]);
 
         // $user = UserModel::where('username','manager')->firstOrFail();
+        // $user = UserModel::findOurFail(1);
+        // $user = UserModel::findOr(1, ['username', 'nama'], function(){
+        //     abort(404);
+        // }); //coba kalau idnya 20
+        // $user = UserModel::firstWhere('level_id',1);
+        // $user = UserModel::where('level_id',1)->first();
+        // $user = UserModel::find(1);
         // return view('user', ['data' => $user]);
         
         // //tambah data user dengan Eloquent Model
@@ -22,6 +64,12 @@ class UserController extends Controller
         //     'level_id' => 2,
         //     'username' => 'manager_tiga',
         //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        // ];
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_dua',
+        //     'nama' => 'Manager 2',
         //     'password' => Hash::make('12345'),
         // ];
         // UserModel::create($data);
