63293771c6c3486b8692f775b009bd57f444e7fd
Ereen Lourenza Natalia Mamahi <ereenlourenza@gmail.com>
2024-05-07 15:27:17 +0700

    js9_auth dan middleware

--- Pertemuan 3/PWL_POS/app/Http/Controllers/AuthController.php
+++ Pertemuan 3/PWL_POS/app/Http/Controllers/AuthController.php
@@ -9,84 +9,185 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Validator as FacadesValidator;
 
 class AuthController extends Controller
 {
     public function index(){
-        return view('auth.login');
+        // kita ambil data user lalu simpan pada variable $user
+        $user = Auth::user();
+
+        // kondisi jika user nya ada
+        if ($user){
+            // jika user nya memiliki level admin
+            if ($user->level_id == '1'){
+                return redirect()->intended('admin');
+            }   
+            // jika user nya memiliki level manager
+            else if ($user->level_id == '2'){
+                return redirect()->intended('manager');
+            }
+        }
+        return view('login');
     }
 
-    public function authenticate(Request $request){
-        $credentials = $request->validate([
-            'username' => ['required'],
-            'password' => ['required'],
+    public function proses_login(Request $request){
+        // kita buat validasi pada saat tombol login di klik
+        // validasi nya username & password wajib di isi
+        $request->validate([
+            'username' => 'required',
+            'password' => 'required'
         ]);
- 
-        if (Auth::attempt($credentials)) {
-            $user = UserModel::where('username', $credentials['username'])->first();
-            
-            if($user->status == 0)  return back()->withErrors(['status' => 'Akun Anda belum divalidasi.']);
 
-            $request->session()->regenerate();
- 
-            return redirect()->route('beranda.index');
+        // ambil data request username & password saja
+        $credential = $request->only('username', 'password');
+        // cek jika data username dan password valid (sesuai) dengan data
+        if (Auth::attempt($credential)){
+            // kalau berhasil simpan data user nya di variabel $user
+            $user = Auth::user();
+
+            // cek lagi jika level user admin maka arahkan ke halaman admin
+            if ($user->level_id == '1'){
+                //dd($user->level_id);
+                return redirect()->intended('admin');
+            }
+            //tapi jika level user nya user biasa maka arahkan ke halaman user
+            else if ($user->level_id == '2'){
+                return redirect()->intended('manager');
+            }
+            // jika belum ada role maka ke halaman /
+            return redirect()->intended('/');
         }
- 
-        return back()->withErrors([
-            'authentication' => 'Username/Password invalid',
-        ])->onlyInput('username');
+        // jika ga ada data user yang valid maka kembalikan lagi ke halaman login
+        // pastikan kirim pesan error juga kalau login gagal ya
+        return redirect('login')
+            ->withInput()
+            ->withErrors(['login_gagal' => 'Pastikan kembali username dan password yang dimasukkan sudah benar']);
+    }
+
+    public function register(){
+        // tampilkan view register
+        return view('register');
+    }
+
+    // aksi form register
+    public function proses_register(Request $request){
+        // validasi semua field wajib di isi 
+        // validasi username unique
+        $validator = FacadesValidator::make($request->all(),[
+            'nama'=> 'required',
+            'username'=>'required|unique:m_user',
+            'password'=>'required'
+        ]);
+
+        // kalau gagal kembali ke halaman register dan muncul pesna error 
+        if ($validator ->fails()) {
+            return redirect('/regsiter')
+            ->withErrors($validator)
+            ->withInput();
+        }
+
+        // kalau berhasil isi level dan hash passwordnya biar secure
+        $request['level_id']='2';
+        $request['password'] = Hash::make($request -> password);
+
+        // masukkan semua data pada request ke tabel m_user
+        UserModel::create($request->all());
+
+        // kalo berhasil arahkan ke halaman login 
+        return redirect()->route('login');
     }
 
     public function logout(Request $request){
+        // logout itu harus menghapus sessionnya 
+        $request->session()->flush();
+
+        // jalankan fungsi logout pada auth
         Auth::logout();
-    
-        $request->session()->invalidate();
-    
-        $request->session()->regenerateToken();
-    
-        return redirect()->route('login.index');
+
+        // kembali ke halaman login 
+        return redirect('login');
     }
 
-    public function register()
-    {
-        return view('auth.register');
-    }
+    // =============================================================
+    // =============================UTS=============================
+    // =============================================================
 
-    public function storeMember(Request $request): RedirectResponse //: RedirectResponse is return type
-    {
-        $newUser = $request->validate([
-            'username' => ['required', 'unique:m_user,username'],
-            'nama' => ['required'],
-            'password' => ['required', 'min:6'],
-            'confirm_password' => ['required', 'same:password'],
-            'profile_img' => ['required', 'mimes:png,jpg,jpeg', 'max:1024'],
-        ]);
+    // public function index(){
+    //     return view('auth.login');
+    // }
 
-        $newUser['level_id'] = 4; //member
-        $newUser['status'] = 0; //harus divalidasi
-
-        try {
-            DB::beginTransaction();
-
-            // Store profile image
-            $profileImg = $newUser['profile_img'];
-            $profileName = Str::random(10).$newUser['profile_img']->getClientOriginalName();
-            $profileImg->storeAs('public/profile', $profileName);
-
-            // Overide profile_img name
-             $newUser['profile_img'] = $profileName;
+    // public function authenticate(Request $request){
+    //     $credentials = $request->validate([
+    //         'username' => ['required'],
+    //         'password' => ['required'],
+    //     ]);
+ 
+    //     if (Auth::attempt($credentials)) {
+    //         $user = UserModel::where('username', $credentials['username'])->first();
             
-            userModel::create($newUser);
+    //         if($user->status == 0)  return back()->withErrors(['status' => 'Akun Anda belum divalidasi.']);
 
-            DB::commit();
-            return redirect()->route('login.index')->with('success', 'Registrasi telah disimpan, tetapi dimohon menunggu untuk validasi akun oleh Admin.');
+    //         $request->session()->regenerate();
+ 
+    //         return redirect()->route('beranda.index');
+    //     }
+ 
+    //     return back()->withErrors([
+    //         'authentication' => 'Username/Password invalid',
+    //     ])->onlyInput('username');
+    // }
 
-        } catch (\Throwable $th) {
-            DB::rollback();
+    // public function logout(Request $request){
+    //     Auth::logout();
+    
+    //     $request->session()->invalidate();
+    
+    //     $request->session()->regenerateToken();
+    
+    //     return redirect()->route('login.index');
+    // }
 
-            return back()->withErrors([
-                'error' => $th->getMessage(),
-            ]);
-        }
-    }
+    // public function register()
+    // {
+    //     return view('auth.register');
+    // }
+
+    // public function storeMember(Request $request): RedirectResponse //: RedirectResponse is return type
+    // {
+    //     $newUser = $request->validate([
+    //         'username' => ['required', 'unique:m_user,username'],
+    //         'nama' => ['required'],
+    //         'password' => ['required', 'min:6'],
+    //         'confirm_password' => ['required', 'same:password'],
+    //         'profile_img' => ['required', 'mimes:png,jpg,jpeg', 'max:1024'],
+    //     ]);
+
+    //     $newUser['level_id'] = 4; //member
+    //     $newUser['status'] = 0; //harus divalidasi
+
+    //     try {
+    //         DB::beginTransaction();
+
+    //         // Store profile image
+    //         $profileImg = $newUser['profile_img'];
+    //         $profileName = Str::random(10).$newUser['profile_img']->getClientOriginalName();
+    //         $profileImg->storeAs('public/profile', $profileName);
+
+    //         // Overide profile_img name
+    //          $newUser['profile_img'] = $profileName;
+            
+    //         userModel::create($newUser);
+
+    //         DB::commit();
+    //         return redirect()->route('login.index')->with('success', 'Registrasi telah disimpan, tetapi dimohon menunggu untuk validasi akun oleh Admin.');
+
+    //     } catch (\Throwable $th) {
+    //         DB::rollback();
+
+    //         return back()->withErrors([
+    //             'error' => $th->getMessage(),
+    //         ]);
+    //     }
+    // }
 }
--- Pertemuan 3/PWL_POS/routes/web.php
+++ Pertemuan 3/PWL_POS/routes/web.php
@@ -1,13 +1,16 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\ManagerController;
 use App\Http\Controllers\PenjualanController;
 use App\Http\Controllers\StokController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
+use Illuminate\Auth\Events\Logout;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Laravel\Ui\AuthCommand;
@@ -27,92 +30,112 @@ use Laravel\Ui\AuthCommand;
 //     return view('welcome');
 // });
 
-Route::get('/login', [AuthController::class, 'index'])->name('login.index');
-Route::post('/login', [AuthController::class, 'authenticate'])->name('login.authenticate');
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+// =============================Jobsheet 9==========================
 
-Route::get('/register', [AuthController::class, 'register'])->name('register.index');
-Route::post('/register', [AuthController::class, 'storeMember'])->name('register.storeMember');
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::get('register', [AuthController::class, 'register'])->name('register');
+Route::post('proses_login', [AuthController::class, 'proses_login'])->name('proses_login');
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('proses_register', [AuthController::class, 'proses_register'])->name('proses_register');
 
-Route::middleware(['auth'])->group(function(){
-    
-    Route::get('/', [WelcomeController::class, 'index'])->name('beranda.index');
-
-    Route::group(['prefix' => 'member'], function(){
-        Route::post('/list', [WelcomeController::class, 'list'])->name('member.list');
-        Route::get('/export-pdf', [WelcomeController::class, 'exportPdf'])->name('member.export.pdf');
-        Route::get('/export-excel', [WelcomeController::class, 'exportExcel'])->name('member.export.excel');
-        Route::get('/updateValidation/{id}', [WelcomeController::class, 'updateValidation'])->name('member.updateValidation');
-        Route::get('/{id}', [WelcomeController::class, 'show'])->name('member.show');
-        Route::delete('/{id}', [WelcomeController::class, 'destroy'])->name('member.destroy');
+Route::group(['middleware'=> ['auth']], function(){
+    Route::group(['middleware'=>['cek_login:1']], function(){
+        Route::resource('admin', AdminController::class);
+    });
+    Route::group(['middleware'=>['cek_login:2']], function(){
+        Route::resource('admin', ManagerController::class);
     });
 
-    Route::group(['prefix' => 'user'], function(){
-        Route::get('/', [UserController::class, 'index']);          //menampilkan halaman awal user
-        Route::post('/list', [UserController::class, 'list']);      //menampilkan data user dalam bentuk json untuk datatables
-        Route::get('/create', [UserController::class, 'create']);   //menampilkan halaman form tambah user
-        Route::post('/', [UserController::class, 'store']);         //menyimpan data user baru
-        Route::get('/{id}', [UserController::class, 'show']);       //menampilkan detail user
-        Route::get('/{id}/edit', [UserController::class, 'edit']);  //menampilkan halaman form edit user
-        Route::put('/{id}', [UserController::class, 'update']);     //menyimpan perubahan data user
-        Route::delete('/{id}', [UserController::class, 'destroy']); //menghapus data user
-    });
-
-    Route::group(['prefix' => 'level'], function(){
-        Route::get('/', [LevelController::class, 'index']);
-        Route::post('/list', [LevelController::class, 'list']);
-        Route::get('/create', [LevelController::class, 'create']);
-        Route::post('/', [LevelController::class, 'store']);
-        Route::get('/{id}', [LevelController::class, 'show']);
-        Route::get('/{id}/edit', [LevelController::class, 'edit']);
-        Route::put('/{id}', [LevelController::class, 'update']);
-        Route::delete('/{id}', [LevelController::class, 'destroy']);
-    });
-
-    Route::group(['prefix' => 'kategori'], function(){
-        Route::get('/', [KategoriController::class, 'index']);
-        Route::post('/list', [KategoriController::class, 'list']);
-        Route::get('/create', [KategoriController::class, 'create']);
-        Route::post('/', [KategoriController::class, 'store']);
-        Route::get('/{id}', [KategoriController::class, 'show']);
-        Route::get('/{id}/edit', [KategoriController::class, 'edit']);
-        Route::put('/{id}', [KategoriController::class, 'update']);
-        Route::delete('/{id}', [KategoriController::class, 'destroy']);
-    });
-
-    Route::group(['prefix' => 'barang'], function(){
-        Route::get('/', [BarangController::class, 'index']);
-        Route::post('/list', [BarangController::class, 'list']);
-        Route::get('/create', [BarangController::class, 'create']);
-        Route::post('/', [BarangController::class, 'store']);
-        Route::get('/{id}', [BarangController::class, 'show']);
-        Route::get('/{id}/edit', [BarangController::class, 'edit']);
-        Route::put('/{id}', [BarangController::class, 'update']);
-        Route::delete('/{id}', [BarangController::class, 'destroy']);
-    });
-
-    Route::group(['prefix' => 'stok'], function(){
-        Route::get('/', [StokController::class, 'index']);
-        Route::post('/list', [StokController::class, 'list']);
-        Route::get('/create', [StokController::class, 'create']);
-        Route::post('/', [StokController::class, 'store']);
-        Route::get('/{id}', [StokController::class, 'show']);
-        Route::get('/{id}/edit', [StokController::class, 'edit']);
-        Route::put('/{id}', [StokController::class, 'update']);
-        Route::delete('/{id}', [StokController::class, 'destroy']);
-    });
-
-    Route::group(['prefix' => 'penjualan'], function(){
-        Route::get('/', [PenjualanController::class, 'index']);
-        Route::get('/list', [PenjualanController::class, 'list']);
-        Route::get('/create', [PenjualanController::class, 'create']);
-        Route::post('/', [PenjualanController::class, 'store']);
-        Route::get('/{id}', [PenjualanController::class, 'show']);
-        Route::get('/{id}/edit', [PenjualanController::class, 'edit']);
-        Route::put('/{id}', [PenjualanController::class, 'update']);
-        Route::delete('/{id}', [PenjualanController::class, 'destroy']);
-    });
 });
+
+// =============================UTS=============================
+
+// Route::get('/login', [AuthController::class, 'index'])->name('login.index');
+// Route::post('/login', [AuthController::class, 'authenticate'])->name('login.authenticate');
+// Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Route::get('/register', [AuthController::class, 'register'])->name('register.index');
+// Route::post('/register', [AuthController::class, 'storeMember'])->name('register.storeMember');
+
+// Route::middleware(['auth'])->group(function(){
+    
+//     Route::get('/', [WelcomeController::class, 'index'])->name('beranda.index');
+
+//     Route::group(['prefix' => 'member'], function(){
+//         Route::post('/list', [WelcomeController::class, 'list'])->name('member.list');
+//         Route::get('/export-pdf', [WelcomeController::class, 'exportPdf'])->name('member.export.pdf');
+//         Route::get('/export-excel', [WelcomeController::class, 'exportExcel'])->name('member.export.excel');
+//         Route::get('/updateValidation/{id}', [WelcomeController::class, 'updateValidation'])->name('member.updateValidation');
+//         Route::get('/{id}', [WelcomeController::class, 'show'])->name('member.show');
+//         Route::delete('/{id}', [WelcomeController::class, 'destroy'])->name('member.destroy');
+//     });
+
+//     Route::group(['prefix' => 'user'], function(){
+//         Route::get('/', [UserController::class, 'index']);          //menampilkan halaman awal user
+//         Route::post('/list', [UserController::class, 'list']);      //menampilkan data user dalam bentuk json untuk datatables
+//         Route::get('/create', [UserController::class, 'create']);   //menampilkan halaman form tambah user
+//         Route::post('/', [UserController::class, 'store']);         //menyimpan data user baru
+//         Route::get('/{id}', [UserController::class, 'show']);       //menampilkan detail user
+//         Route::get('/{id}/edit', [UserController::class, 'edit']);  //menampilkan halaman form edit user
+//         Route::put('/{id}', [UserController::class, 'update']);     //menyimpan perubahan data user
+//         Route::delete('/{id}', [UserController::class, 'destroy']); //menghapus data user
+//     });
+
+//     Route::group(['prefix' => 'level'], function(){
+//         Route::get('/', [LevelController::class, 'index']);
+//         Route::post('/list', [LevelController::class, 'list']);
+//         Route::get('/create', [LevelController::class, 'create']);
+//         Route::post('/', [LevelController::class, 'store']);
+//         Route::get('/{id}', [LevelController::class, 'show']);
+//         Route::get('/{id}/edit', [LevelController::class, 'edit']);
+//         Route::put('/{id}', [LevelController::class, 'update']);
+//         Route::delete('/{id}', [LevelController::class, 'destroy']);
+//     });
+
+//     Route::group(['prefix' => 'kategori'], function(){
+//         Route::get('/', [KategoriController::class, 'index']);
+//         Route::post('/list', [KategoriController::class, 'list']);
+//         Route::get('/create', [KategoriController::class, 'create']);
+//         Route::post('/', [KategoriController::class, 'store']);
+//         Route::get('/{id}', [KategoriController::class, 'show']);
+//         Route::get('/{id}/edit', [KategoriController::class, 'edit']);
+//         Route::put('/{id}', [KategoriController::class, 'update']);
+//         Route::delete('/{id}', [KategoriController::class, 'destroy']);
+//     });
+
+//     Route::group(['prefix' => 'barang'], function(){
+//         Route::get('/', [BarangController::class, 'index']);
+//         Route::post('/list', [BarangController::class, 'list']);
+//         Route::get('/create', [BarangController::class, 'create']);
+//         Route::post('/', [BarangController::class, 'store']);
+//         Route::get('/{id}', [BarangController::class, 'show']);
+//         Route::get('/{id}/edit', [BarangController::class, 'edit']);
+//         Route::put('/{id}', [BarangController::class, 'update']);
+//         Route::delete('/{id}', [BarangController::class, 'destroy']);
+//     });
+
+//     Route::group(['prefix' => 'stok'], function(){
+//         Route::get('/', [StokController::class, 'index']);
+//         Route::post('/list', [StokController::class, 'list']);
+//         Route::get('/create', [StokController::class, 'create']);
+//         Route::post('/', [StokController::class, 'store']);
+//         Route::get('/{id}', [StokController::class, 'show']);
+//         Route::get('/{id}/edit', [StokController::class, 'edit']);
+//         Route::put('/{id}', [StokController::class, 'update']);
+//         Route::delete('/{id}', [StokController::class, 'destroy']);
+//     });
+
+//     Route::group(['prefix' => 'penjualan'], function(){
+//         Route::get('/', [PenjualanController::class, 'index']);
+//         Route::get('/list', [PenjualanController::class, 'list']);
+//         Route::get('/create', [PenjualanController::class, 'create']);
+//         Route::post('/', [PenjualanController::class, 'store']);
+//         Route::get('/{id}', [PenjualanController::class, 'show']);
+//         Route::get('/{id}/edit', [PenjualanController::class, 'edit']);
+//         Route::put('/{id}', [PenjualanController::class, 'update']);
+//         Route::delete('/{id}', [PenjualanController::class, 'destroy']);
+//     });
+// });
 
 
 // Route::get('/level', [LevelController::class, 'index']);
