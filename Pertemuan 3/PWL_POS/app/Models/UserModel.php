40c68851f24a20b8ed40b528c0113330afb17d52
Ereen Lourenza Natalia Mamahi <ereenlourenza@gmail.com>
2024-05-20 15:41:08 +0700

    js11_p1_implementasi eloquent accessor

--- Pertemuan 3/PWL_POS/app/Models/UserModel.php
+++ Pertemuan 3/PWL_POS/app/Models/UserModel.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+// use Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
 class UserModel extends Authenticatable implements JWTSubject
@@ -28,7 +30,7 @@ class UserModel extends Authenticatable implements JWTSubject
      * 
      * @var array
      */
-    protected $fillable = ['level_id','username','nama','password', 'status', 'profile_img'];
+    protected $fillable = ['level_id','username','nama','password', 'status', 'profile_img', 'image'];
 
     public function level(): BelongsTo{
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
@@ -37,6 +39,11 @@ class UserModel extends Authenticatable implements JWTSubject
     // public function stok(): HasMany{
     //     return $this->hasMany(StokModel::class, 'stok_id', 'stok_id');
     // }
+    protected function image(): Attribute{
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' . $image)
+        );
+    }
 
     protected $hidden = [
         'password'
