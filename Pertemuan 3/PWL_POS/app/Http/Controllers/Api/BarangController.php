f32651e8476f90b9102dcae929742adee7d655b2
Ereen Lourenza Natalia Mamahi <ereenlourenza@gmail.com>
2024-05-20 16:55:32 +0700

    js11_tugas

--- Pertemuan 3/PWL_POS/app/Http/Controllers/Api/BarangController.php
+++ Pertemuan 3/PWL_POS/app/Http/Controllers/Api/BarangController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\BarangModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class BarangController extends Controller
 {
@@ -12,10 +13,50 @@ class BarangController extends Controller
         return BarangModel::all();
     }
 
+    // public function store(Request $request){
+    //     $barang = BarangModel::create($request->all());
+    //     return response()->json($barang, 201);
+    // }   
     public function store(Request $request){
-        $barang = BarangModel::create($request->all());
-        return response()->json($barang, 201);
-    }   
+        
+        //set validation
+        $validator = Validator::make($request->all(),[
+            'barang_kode' => 'required',
+            'barang_nama' => 'required',
+            'harga_beli' => 'required|integer',
+            'harga_jual' => 'required|integer',
+            'kategori_id' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        //if validations fails
+        if($validator->fails()){
+            return response()->json($validator->errors(), 422);
+        }
+
+        //create barang
+        $barang = BarangModel::create([
+            'barang_kode' => $request->barang_kode,
+            'barang_nama' => $request->barang_nama,
+            'harga_beli' => $request->harga_beli,
+            'harga_jual' => $request->harga_jual,
+            'kategori_id' => $request->kategori_id,
+            'image' => $request->image->hashName(),
+        ]);
+
+        //return response JSON barang is created
+        if($barang){
+            return response()->json([
+                'success' => true,
+                'barang' => $barang,
+            ], 201);
+        }
+
+        //return JSON process insert failed
+        return response()->json([
+            'success' => false,
+        ], 409);
+    }
 
     public function show(BarangModel $barang){
         return $barang;
